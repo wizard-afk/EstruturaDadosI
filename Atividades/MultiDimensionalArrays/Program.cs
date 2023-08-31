e1fc37c0cc53238fb1ac10921a193cdb712370ef
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-08-30 22:15:17 -0300

    Aula Tres

---
+++ Atividades/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿int[,] biNumbers = new int[5,2];
+
+int[,,] triNumbers = new int[5,4,3];
+
+
+int[,,] loadedBiNumbers = new int[,,]
+{
+    {  { 9,   5 , 15 }, { 9,   5 , 15 }, { 9,   5 , 15 } },
+    {  {10, -11 , 15 }, { 9,   5 , 15 }, { 9,   5 , 15 } },
+    {  {101,  45 , 8 }, { 9,   5 , 15 }, { 9,   5 , 15 } }
+};
+
+for(int i = 0; i < loadedBiNumbers.GetLength(0); i++)
+{
+    for(int j = 0; j < loadedBiNumbers.GetLength(1); j++)
+    {
+        for(int z = 0; z < loadedBiNumbers.GetLength(2); z++)
+        Console.Write( 
+            $"[{loadedBiNumbers[i,j,z]}]" 
+        );
+    }
+    Console.WriteLine("");
+}
+
+
