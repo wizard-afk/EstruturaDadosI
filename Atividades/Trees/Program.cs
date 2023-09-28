61f48b50fffbd3556956d48a577ef3bdab73597e
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-09-27 22:07:19 -0300

    Arvores

---
+++ Atividades/Trees/Program.cs
@@ -0,0 +1,50 @@
+﻿using Trees;
+
+Tree<int> tree = new Tree<int>();
+// Inicializa o nó Raiz com o valor 1000
+tree.Root = new TreeNode<int>() { 
+    Data = 1000 
+};
+// Troca o valor de Data no nó Raiz
+tree.Root.Data = 100;
+//Informa carga de nós filhos
+tree.Root.Children = new List<TreeNode<int>>
+{
+    new TreeNode<int>() { 
+        Data = 50, 
+        Parent = tree.Root
+    },
+    new TreeNode<int>() { 
+        Data = 5, 
+        Parent = tree.Root
+    }    
+};
+// Adicionando nó em tempo de execução
+TreeNode<int> filho3 = new TreeNode<int>();
+filho3.Data = 0;
+filho3.Parent = tree.Root;
+tree.Root.Children.Add(filho3);
+
+//Adicionando netos de Root, ou seja,
+// filhos do primeiro nível de hereditariedade
+tree.Root.Children[0].Children = 
+    new List<TreeNode<int>>
+{
+    new TreeNode<int>() { 
+        Data = 55, 
+        Parent = tree.Root.Children[0]
+    }
+};
+
+//Adicionando netos de root no segundo filho
+TreeNode<int> neto2 = new TreeNode<int>();
+neto2.Data = 200;
+neto2.Parent = tree.Root.Children[1];
+
+tree.Root.Children[1].Children = 
+new List<TreeNode<int>>();
+tree.Root.Children[1].Children.Add(neto2);
+
+int level = neto2.GetHeight();
+string message = $"Esta árvore contém {level} níveis";
+Console.WriteLine(message);
---
+++ Atividades/Trees/TreeNode.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }       
+        //Atributo recursivo
+        public TreeNode<T> Parent { get; set; }
+        // Lista com chamada de tipo recursivo
+        public List< TreeNode<T> > Children { get; set; }
+
+        public int GetHeight()
+        {
+            int height = 1;
+            TreeNode<T> current = this;
+            while(current.Parent != null)
+            {
+                height++;
+                current = current.Parent;
+            }
+
+            return height;
+        }
+    }
+}
