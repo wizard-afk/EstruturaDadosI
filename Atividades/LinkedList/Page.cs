1513425333ac5c298955f41ede91781b6a9673b6
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-10-25 22:04:54 -0300

    LinkedList

---
+++ Atividades/LinkedList/Page.cs
@@ -0,0 +1,8 @@
+namespace LinkedList
+{
+    public class Page
+    {
+        public string? Content { get; set; }
+        public int? PageNumber { get; set; }
+    }
+}
---
+++ Atividades/LinkedList/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using LinkedList;
+
+Page pageFirst = new Page() { Content = "Atualmente..." };
+Page pageSecond = new Page() { Content = "Aplicação..."};
+Page pageThird = new Page() { Content = "Muito de..."};
+
+Page pageFourth = new Page() { Content = "Você sabia...", PageNumber = 4};
+pageFourth.PageNumber = 4;
+
+Page pageFifth = new Page() { 
+    Content = "Enquanto...",
+    PageNumber = 5
+};
+
+Page pageSixth = new Page();
+pageSixth.Content = "Você poderia...";
+pageSixth.PageNumber = 6;
+
+var pageSeventh = new Page();
+pageSeventh.Content = "Era uma vez...";
+pageSeventh.PageNumber = 7;
+
+LinkedList<Page> pages = new LinkedList<Page>();
+pages.AddLast(pageSecond);
+LinkedListNode<Page> nodePageFourth = 
+    pages.AddLast(pageFourth);
+pages.AddLast(pageSixth);
+pages.AddFirst(pageFirst);
+
+pages.AddBefore(nodePageFourth, pageThird);
+pages.AddAfter(nodePageFourth, pageFifth);
+
